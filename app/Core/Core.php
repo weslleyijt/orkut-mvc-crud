50a3aa73462064395f49623cffe3cf9389582e7d
Weslley <65179488+Escolabebyte@users.noreply.github.com>
2021-09-27 00:36:04 -0300

    att

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -1,38 +1,54 @@
 <?php
-    namespace App\Core; 
-    
+    namespace App\Core;   
+    use App\Model\User as User;
+
     class Core
     {
         public function start($urlGet)
-        {  
-            if(isset($urlGet)) 
-            {
-                $strArray = explode('/', $urlGet);   
-                $controller = ucfirst($strArray[0] . "Controller"); 
-            } 
-            else
-                $controller = "HomeController";
-            
-            if(!class_exists($controller))
-                $controller = "HomeController"; 
+        {   
+            $loader = new \Twig\Loader\FilesystemLoader('app/Template'); 
+            $twig = new \Twig\Environment($loader); 
 
-            $this->startFunction($controller);
-        }
+            if(User::is_user_logged_in())  
+            { 
+                $template =  $twig->load('template.html');
+
+                if(isset($urlGet))  
+                {
+                    $strArray = explode('/', $urlGet);   
+                    $controller = ucfirst($strArray[0] . "Controller"); 
+                } 
+                else
+                    $controller = "HomeController";
+                
+                if(!class_exists($controller))
+                    $controller = "HomeController"; 
  
-        public function startUserProfile()
-        {
-            $this->startFunction("ProfileController");
-        }
-        
-        public function startSidePanel($urlGet) 
-        {
-            $this->startFunction("PanelController");  
-        }
+                $params['user_profile'] = $this->user_func('ProfileController', $urlGet);
+                $params['dynamic_area'] = $this->user_func($controller, $urlGet);
+                $params['panel_side'] = $this->user_func('PanelController', $urlGet); 
+                $params['user_email'] = "email@gmail.com"; 
 
-        public function startFunction($controller)
+                $content = $template->render($params);
+                echo $content; 
+            }
+            else
+            {
+                $template =  $twig->load('login-template.html'); 
+                $content = $template->render();
+                echo $content; 
+            } 
+        } 
+
+        public function user_func($controller, $params)  
         {
             $action = "index"; 
             $tmp = 'App\Controller\\' . $controller; 
-            call_user_func_array(array(new $tmp, $action), array());  
+
+            ob_start();
+                call_user_func_array(array(new $tmp, $action), array($params));   
+                $user_func = ob_get_contents(); 
+            ob_end_clean();
+            return $user_func; 
         }
     }
---
+++ app/Model/User.php
@@ -0,0 +1,10 @@
+<?php    
+    namespace App\Model; 
+    
+    class User 
+    {
+        public static function is_user_logged_in() 
+        { 
+            return isset($_SESSION['user_id']); 
+        }   
+    }  
